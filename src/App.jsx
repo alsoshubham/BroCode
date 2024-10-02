a73331e3a91b4840ba99654463fbb8259499a633
alsoshubham <shubhamsahu100100@hotmail.com>
2024-10-03 03:24:49 +0530

    created an array of nested objects, reframed the structure of app and components workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { CartProvider } from "./Context/CartContext";
-import Home from "./Pages/Home";
+import Menu from "./Menu";
 import "./App.css";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <CartProvider>
-      <Home />
-    </CartProvider>
+    <div>
+      <div>
+        <h2>BroCode</h2>
+        <div>
+      <Routes>
+        <Route path="/Noodles" element={Noodles}/>
+        <Route path="/Momo" element={MOMO}/>
+        <Route path="/Pasta" element={Pasta}/>
+        <Route path="/Chilli Potato" element={ChilliPotato}/>
+        <Route path="/Drinks" element={Mocktails}/>
+        <Route path="/Rice" element={Rice}/>
+        <Route path="/Manchurian" element={Manchurian}/>
+      </Routes>
+      </div>
+      <Route path="/Menu" element={Menu}/>
+      </div>
+    </div>
   );
 }
 
