4b6a22174c9b93ee0d5332225ad162ca190e5ae0
alsoshubham <shubhamsahu100100@hotmail.com>
2024-10-04 23:26:23 +0530

    while initiating chatbot it messed so scrapped the previous approach and starting with new approach,

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-import Menu from "./Menu";
-import "./App.css";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
 
-
-function App() {
+export const App = () => {
   return (
-    <div>
-      <div>
-        <h2>BroCode</h2>
-        <div>
-      <Routes>
-        <Route path="/Noodles" element={Noodles}/>
-        <Route path="/Momo" element={MOMO}/>
-        <Route path="/Pasta" element={Pasta}/>
-        <Route path="/Chilli Potato" element={ChilliPotato}/>
-        <Route path="/Drinks" element={Mocktails}/>
-        <Route path="/Rice" element={Rice}/>
-        <Route path="/Manchurian" element={Manchurian}/>
-      </Routes>
-      </div>
-      <Route path="/Menu" element={Menu}/>
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
